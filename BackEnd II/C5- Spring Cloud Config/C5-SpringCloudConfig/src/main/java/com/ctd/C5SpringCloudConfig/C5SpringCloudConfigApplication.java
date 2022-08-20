87236917a7aa994c811b3d81d0a173aceafc91a8
agvanetta <agvanetta@gmail.com>
2022-08-20 17:55:06 -0300

    push

--- BackEnd II/C5- Spring Cloud Config/C5-SpringCloudConfig/src/main/java/com/ctd/C5SpringCloudConfig/C5SpringCloudConfigApplication.java
+++ BackEnd II/C5- Spring Cloud Config/C5-SpringCloudConfig/src/main/java/com/ctd/C5SpringCloudConfig/C5SpringCloudConfigApplication.java
@@ -2,8 +2,10 @@ package com.ctd.C5SpringCloudConfig;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class C5SpringCloudConfigApplication {
 
 	public static void main(String[] args) {
