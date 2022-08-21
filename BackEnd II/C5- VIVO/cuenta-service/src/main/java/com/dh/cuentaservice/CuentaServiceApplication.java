c097b727727b055b1a51ad9aeac3bb97ec453632
agvanetta <agvanetta@gmail.com>
2022-08-20 22:10:32 -0300

    springconfig

---
+++ BackEnd II/C5- VIVO/cuenta-service/src/main/java/com/dh/cuentaservice/CuentaServiceApplication.java
@@ -0,0 +1,13 @@
+package com.dh.cuentaservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CuentaServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CuentaServiceApplication.class, args);
+	}
+
+}
